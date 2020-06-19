e66c06c0d58bd5955176a43ab316d6eccdb32ecb
DevTotti <devtotti@gmail.com>
2020-06-19 10:43:37 +0100

    adding new swagger

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, request, render_template
 from datetime import datetime
+from flasgger import Swagger
+from flasgger.utils import swag_from
 from fetchData import *
 import schedule as sc
 from optimized import *
@@ -8,9 +10,29 @@ from optimizedOffer import *
 
 
 app = Flask(__name__)
+app.config['SWAGGER'] = {"title":"Swagger-UI", "version":2}
+
+swagger_config = {
+	"headers":[],
+	"specs":[
+		{
+			"endpoint":"apispec_1",
+			"route":"/apispec_1.json",
+			"rule_filter":lambda rule: True,
+			"model_filter": lambda tag: True,
+		}
+	],
+	"static_url_path":"/flasgger_static",
+	"swagger_ui":True,
+	"specs_route":"/swagger/",
+}
+
+swagger = Swagger(app, config = swagger_config)
 
 
 @app.route("/fetch", methods = ["GET", "POST"])
+@swag_from("swagger_config.yml", methods = ['POST'])
+@swag_from("swagger_config_get.yml", methods = ['GET'])
 def fetchData():
 
 	if request.method == 'GET':
